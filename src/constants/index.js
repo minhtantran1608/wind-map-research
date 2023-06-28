8d4708c4fbe378b1096eab5a776ecdccde8a2e06
Tran Minh Tan <82354057+minhtantran1608@users.noreply.github.com>
2023-06-28 09:52:27 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,7 @@
 const getMapBoxToken = () => {
   const deployMode = process.env.REACT_APP_DEPLOY_MODE || "development";
-  const devKey =
-    process.env.REACT_APP_MAPBOX_DEV_KEY ||
-    "pk.eyJ1IjoicmVlY290ZWNoLW1hcGJveCIsImEiOiJjbDNpaTFoZHgwNDhpM2lvOHN0NTZmc2hsIn0.XjrYjmewpbxhqvrn-9QqpA";
-  const prodKey =
-    process.env.REACT_APP_MAPBOX_PROD_KEY ||
-    "pk.eyJ1IjoicmVlY290ZWNoLW1hcGJveCIsImEiOiJjbDNqaDRnNHkwZWJuM2JxaWZhcGdteWxjIn0.e5wTJ5qAlgScJBcWlKMUmQ";
+  const devKey = process.env.REACT_APP_MAPBOX_DEV_KEY;
+  const prodKey = process.env.REACT_APP_MAPBOX_PROD_KEY;
 
   if (deployMode === "production") {
     return prodKey;
